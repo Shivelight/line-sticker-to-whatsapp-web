ee8106355a771b34c99cdebdd47c3b2f56658ddf
Shivelight <shivelight@tuta.io>
2023-02-19 17:27:25 +0700

    Add help page

--- src/lib/Help.svelte
+++ src/lib/Help.svelte
@@ -1,11 +1,55 @@
 <p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet tempora velit
-  voluptatibus sit quo? Rerum laudantium rem soluta atque ipsum necessitatibus
-  quam optio! Veritatis, placeat exercitationem? Nesciunt vero odio a.
+  First you need to get LINE sticker pack ID or store URL that you want to convert either from <a href="https://store.line.me/" target="_blank" rel="noreferrer">store.line.me</a> or from the app.
 </p>
+<div class="left">
+  <p>Example:</p>
+  <pre>1. https://store.line.me/stickershop/product/<span>1344676</span> (Store URL)</pre>
+  <pre>2. https://line.me/S/sticker/<span>1344676</span> (App URL)</pre>
+  <pre>3. <span>1344676</span> (ID Only)</pre>
+  <pre><span>*</span> If the URL includes more paths or parameters or seems to be longer than the examples, then don't worry, it is also accepted as long as the ID is present.</pre>
+</div>
+<p>
+  Enter the sticker pack ID or URL into the input field and then press <kbd>Get Data</kbd> button. If the sticker pack is found, its detail will be displayed.
+</p>
+<p>
+  If everything is correct, press <kbd>Convert to WhatsApp Sticker</kbd> button to start the process. Download will start automatically after the process is finished; make sure to confirm if needed.
+</p>
+<h3>Android: What Next?</h3>
+<p>To install the sticker pack into WhatsApp, you need to extract the content of the downloaded <code>.zip</code> file first. Then you can use any of your favorite personal sticker maker to load the sticker pack, but if you don't have any, I recommend <a href="https://play.google.com/store/apps/details?id=com.dstukalov.walocalstoragestickers" target="_blank" rel=" noreferrer">Personal stickers for WhatsApp</a> because of its simplicity.</p>
+<h3>iOS: What Next?</h3>
+<p style="font-style: italic;">TBD (I'm not an iOS user).</p>
 
 <style>
   p {
     text-align: justify;
   }
+
+  span {
+    font-weight: 700;
+    color: greenyellow;
+  }
+
+  pre {
+    white-space: pre-wrap;
+  }
+
+  kbd {
+    background-color: #eee;
+    border-radius: 3px;
+    border: 1px solid #b4b4b4;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2), 0 2px 0 0 rgba(255, 255, 255, 0.7) inset;
+    color: #333;
+    display: inline-block;
+    font-size: 0.85em;
+    font-weight: 700;
+    line-height: 1;
+    padding: 2px 4px;
+    white-space: nowrap;
+  }
+
+  .left {
+    text-align: left;
+    word-wrap: break-word;
+  }
+
 </style>
