1c9d385086684f4f6fc390b57601cf464c9dab7b
Shivelight <shivelight@tuta.io>
2023-02-19 18:50:02 +0700

    Add about page

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import Tabs from "./lib/Tabs.svelte";
   import Convert from "./lib/Convert.svelte";
   import Help from "./lib/Help.svelte";
+  import About from "./lib/About.svelte";
 </script>
 
 <main>
@@ -9,6 +10,7 @@
     tabs={[
       { name: "Convert", content: Convert },
       { name: "Tutorial", content: Help },
+      { name: "About", content: About },
     ]}
   />
 </main>
---
+++ src/lib/About.svelte
@@ -0,0 +1,22 @@
+<p>
+  This tool allow you to download and convert <a href="https://line.me">LINE</a>
+  sticker pack into <a href="https://www.whatsapp.com/">WhatsApp</a> sticker pack.
+</p>
+<div class="container">
+  <a href="https://github.com/Shivelight">My GitHub</a>
+  <a href="https://github.com/Shivelight/line-sticker-to-whatsapp-web"
+    >Source Code</a
+  >
+</div>
+
+<style>
+  p {
+    text-align: justify;
+  }
+
+  .container {
+    font-family: monospace;
+    display: flex;
+    justify-content: space-evenly;
+  }
+</style>
