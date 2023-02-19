9285e6c1a58f5a9eaf4256ed8654fbaca1e720ce
Shivelight <shivelight@tuta.io>
2023-02-19 15:08:45 +0700

    Create tabbed menu and change styling

---
+++ src/lib/Help.svelte
@@ -0,0 +1,11 @@
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet tempora velit
+  voluptatibus sit quo? Rerum laudantium rem soluta atque ipsum necessitatibus
+  quam optio! Veritatis, placeat exercitationem? Nesciunt vero odio a.
+</p>
+
+<style>
+  p {
+    text-align: justify;
+  }
+</style>
---
+++ src/lib/Tabs.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let tabs, activeTab = 0;
+</script>
+
+{#each tabs as tab, idx}
+  <button
+    class="tab"
+    class:active={idx === activeTab}
+    on:click={() => (activeTab = idx)}
+  >
+    {tab.name}
+  </button>
+{/each}
+
+{#each tabs as tab, idx}
+  <div hidden={idx !== activeTab}>
+    <svelte:component this={tab.content} />
+  </div>
+{/each}
+
+<style>
+  .tab {
+    margin: 20px 4px;
+  }
+
+  .tab.active {
+    border: none;
+    color: #00b84f;
+    border-bottom: 2px solid #00b84f;
+  }
+</style>
